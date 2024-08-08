49d275e13c97cf1502e7f2dab538d26e25f3a73a
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-08-08 21:16:30 +0900

    setterの削除

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -13,15 +13,8 @@ public class Student {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public int getScore() {
     return score;
   }
 
-  public void setScore(int score) {
-    this.score = score;
-  }
 }
